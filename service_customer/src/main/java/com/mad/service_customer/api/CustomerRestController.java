d013532007178b7425ced242db6fa798e5fd2da4
Madani <khouane.madani@free.fr>
2022-07-15 17:55:55 +0200

    Modification d'un endpoint. Utilisé un 'ResponseEntity' dans la méthode @GetMapping

--- service_customer/src/main/java/com/mad/service_customer/api/CustomerRestController.java
+++ service_customer/src/main/java/com/mad/service_customer/api/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.mad.service_customer.api;
 import com.mad.service_customer.dto.CustomerRequestDTO;
 import com.mad.service_customer.dto.CustomerResponseDTO;
 import com.mad.service_customer.services.CustomerService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,8 +24,12 @@ public class CustomerRestController {
 
     }
     @GetMapping("/customers/{id}")
-    public CustomerResponseDTO getCustomer(@PathVariable String id) throws Exception {
-        return customerService.getCustomer(id);
+    public ResponseEntity<CustomerResponseDTO> getCustomer(@PathVariable String id) throws Exception {
+        try{
+            return ResponseEntity.ok(customerService.getCustomer(id));
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
     }
     @PostMapping("/customers")
     public CustomerResponseDTO save(@RequestBody CustomerRequestDTO customerRequestDTO){
